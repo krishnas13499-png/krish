d283290aaadb8e225671bbc5094aa16ffef48917
krishnas13499-png <krishnas13499@gmail.com>
2026-08-17 22:42:15 +0530

    Add files via upload

---
+++ Day6.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("A:");
+    scanf("%d",&a);
+
+if(a%2==0){
+    printf(" %d IS EVEN .\n",a);
+}
+else{
+    printf(" %d IS ODD.\n",a);
+}
+
+
+
+
+
+
+}
