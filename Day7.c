ba51cd22aba10c78ea6ab48ab6de49f1e1e1b8a1
krishnas13499-png <krishnas13499@gmail.com>
2026-08-17 22:42:34 +0530

    Add files via upload

---
+++ Day7.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main() {
+    char c;
+    int case1, case2;
+    printf("Enter an alphabet: ");
+    scanf("%c", &c);
+
+    case1 = (c == 'a'||c == 'e'||c == 'i'||c == 'o'||c == 'u');
+
+    case2 = (c == 'A'||c == 'E'||c == 'I'||c == 'O'||c == 'U');
+
+    if (case1 || case2)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
+}
