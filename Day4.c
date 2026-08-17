450e4114dfb973c0f6bbf628b88510afda6150ec
krishnas13499-png <krishnas13499@gmail.com>
2026-08-17 22:40:25 +0530

    Add files via upload

---
+++ Day4.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+        int main(){    
+        int a , b ,
+        sum = 0;
+        printf("ENTER THE NUMBER :");
+        scanf("%d",&a);  
+        printf("THE FIRST %d NATURAL NUMBERS ARE :",a); 
+        for(b=1; b<=a ; b++ )
+        {  
+        printf("%d",b);
+        sum += b;    }  
+        printf("\n THE SUM OF FIRST %d NATURAL NUMBER IS = %d\n",a,sum);
+        return 0;
+      }
