75591bf0584f46efb0de12beec813b32461404e2
krishnas13499-png <krishnas13499@gmail.com>
2026-08-17 22:39:24 +0530

    Add files via upload

---
+++ 1Day.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("A:");
+    scanf("%d",&a);
+    printf("the value of A is:%d\n",a);
+int b;
+printf("B:");
+scanf("%d",&b);
+printf("the value of B is:%d\n",b);
+printf("THE ADDED VALUE IS:%d\n",a+b);
+printf("THE SUBTRACTED VALUE IS:%d\n",a-b);
+printf("THE MULTIPLIED VALUE IS:%d\n",a*b);
+printf("THE DIVIDED VALUE IS:%d\n",a/b);
+return 0;
+
+}
+
+
+    
