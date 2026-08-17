e841dff48e961074195f46791a51c4ec84711354
krishnas13499-png <krishnas13499@gmail.com>
2026-08-17 22:40:06 +0530

    Add files via upload

---
+++ Day3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int a,b;
+    printf("\nBEFORE SWAPPING THE NUMBER:\n");
+    scanf("%d",a);
+    scanf("%d",b);
+     
+    a=b
+    b=a
+     printf("\nAFTER SWAPPING THE NUMBER:\n");
+     scanf("%d",a);
+     scanf("%d",b);
+return 0;
+}
+
