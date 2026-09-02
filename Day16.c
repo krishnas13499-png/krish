17db19eaac988815b4ff36f7b3722103e21f73f4
krishnas13499-png <krishnas13499@gmail.com>
2026-09-02 21:46:56 +0530

    Add files via upload

---
+++ Day16.c
@@ -0,0 +1,35 @@
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder;
+    long reversedNum = 0; 
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    if (num < 0) {
+        printf("%d is not a palindrome.\n", originalNum);
+        return 0;
+    }
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    if (originalNum == reversedNum) {
+        printf("%d is a palindrome.\n", originalNum);
+    } else {
+        printf("%d is not a palindrome.\n", originalNum);
+    }
+
+    return 0;
+}
