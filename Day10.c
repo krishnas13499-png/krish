cc4627878af6085c9739398e09e849cb0c617292
krishnas13499-png <krishnas13499@gmail.com>
2026-09-02 21:31:49 +0530

    Add files via upload

---
+++ Day10.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main(){
+    int a;
+
+    printf("A:");
+    scanf("%d",&a);
+
+    switch(a){
+
+        case 1:
+        printf("MONDAY");
+        break;
+    
+        case 2:
+        printf("TUESDAY");
+         break;
+
+        case 3:
+        printf("WEDNESDAY");
+        break;
+
+        case 4:
+        printf("THURSDAY");
+        break;
+
+        case 5:
+        printf("FRIDAY");
+        break;
+
+        case 6:
+        printf("SATURDAY");
+        break;
+
+        case 7:
+        printf("SUNDAY");
+        break;
+
+    }
+    return 0 ;
+}
+
+
+
+    
+
+
+
+
+
+
+
+
