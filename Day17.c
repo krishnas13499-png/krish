7428eabf42fd84f30d8ad6036e16b14aaa60a33a
krishnas13499-png <krishnas13499@gmail.com>
2026-09-02 21:48:52 +0530

    Add files via upload

---
+++ Day17.c
@@ -0,0 +1,45 @@
+
+
+#include <stdio.h>
+
+
+int main() {
+    int n ;
+    printf("ENTER THE INTEGER:");
+    scanf("%d",&n);
+    int count = 0;
+
+    if (n <= 1) {
+        printf("%d is NOT prime", n);
+
+
+
+
+
+
+    }
+    else {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                count++;
+        }
+
+        if (count == 2)
+            printf("%d is prime", n);
+        else
+            printf("%d is NOT prime", n);
+
+
+
+
+    }
+
+
+
+
+
+
+    return 0;
+}
+
+
